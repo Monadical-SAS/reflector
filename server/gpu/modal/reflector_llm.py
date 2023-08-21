6ee667e595412c0aa8741c43ff896db1f50bb672
Gokul Mohanarangan <gokul@monadical.com>
2023-08-21 23:33:14 +0530

    add LLM features

--- server/gpu/modal/reflector_llm.py
+++ server/gpu/modal/reflector_llm.py
@@ -5,7 +5,7 @@ Reflector GPU backend - LLM
 """
 import json
 import os
-from typing import Optional
+from typing import Callable, List, Optional
 
 from modal import Image, Secret, Stub, asgi_app, method
 
@@ -55,6 +55,7 @@ llm_image = (
         "einops==0.6.1",
         "hf-transfer~=0.1",
         "huggingface_hub==0.16.4",
+        "nltk"
     )
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
     .run_function(download_llm)
@@ -71,6 +72,8 @@ llm_image = (
 )
 class LLM:
     def __enter__(self):
+        import jsonformer
+        import nltk
         import torch
         from transformers import AutoModelForCausalLM, AutoTokenizer
         from transformers.generation import GenerationConfig
@@ -100,7 +103,9 @@ class LLM:
         print("Warmup llm done")
         self.model = model
         self.tokenizer = tokenizer
-        self.gen_cfg = gen_cfg
+        self.base_gen_cfg = GenerationConfig()
+        self.sentence_tokenizer = nltk.sent_tokenize
+        self.json_former = jsonformer.Jsonformer()
 
     def __exit__(self, *args):
         print("Exit llm")
@@ -110,34 +115,93 @@ class LLM:
         print("Warmup ok")
         return {"status": "ok"}
 
-    @method()
-    def generate(self, prompt: str, schema: str = None):
+    @property
+    def supported_tasks(self) -> List[str]:
+        return ["title", "summary", "topic"]
+
+    @property
+    def generate_registry(self) -> None:
+        if not self._generate_registry:
+            self._initialize_generate_registry()
+        return self._generate_registry
+
+    def _initialize_generate_registry(self) -> None:
+        for task in self.supported_tasks:
+            func_name = "_generate_" + task
+            func = getattr(self, func_name, None)
+            if not func:
+                raise NotImplementedError(f"Generation function for '{task}' is not implemented.")
+            self._generate_registry[task] = func
+
+    def _generation_swivel(self, task: str) -> Callable:
+        if task not in self.supported_tasks:
+            raise NotImplementedError(f"Task: '{task}' is not supported.")
+        return self.generate_registry[task]
+
+    def split_corpus(self, corpus: str, token_threshold: int = 500) -> List[str]:
+        total_tokens = 0
+        corpus_sentences = self.sentence_tokenizer(corpus)
+
+        for sentence in corpus_sentences:
+            tokens = self.tokenizer.tokenize(sentence)
+            if total_tokens + len(tokens) <= token_threshold:
+                total_tokens += len(tokens)
+                tokenized_corpus = " ".join(tokens)
+                yield tokenized_corpus
+
+    def _generate_title(self, prompt: str, text: str, schema: str = None) -> str | dict:
+        chunk_titles = []
+        for chunk in self.split_corpus(text):
+            title = self._generate(prompt=prompt, text=chunk, schema=schema, gen_cfg=self.title_gen_cfg)
+            title = title["title"] if schema else title["result"]
+            chunk_titles.append(title)
+
+        collected_titles = " ".join(chunk_titles)
+        return self._generate(prompt=prompt, text=collected_titles, schema=schema, gen_cfg=self.title_gen_cfg)
+
+    def _generate_topic(self, prompt: str, text: str, schema: str = None) -> str | dict:
+        return self._generate(prompt=prompt, text=text, schema=schema, gen_cfg=self.topic_gen_cfg)
+
+    def _generate_summary(self, prompt: str, text: str, schema: str = None) -> str | dict:
+        chunk_summary = []
+        for chunk in self.split_corpus(text):
+            title = self._generate(prompt=prompt, text=chunk, schema=schema, gen_cfg=self.summary_gen_cfg)
+            title = title["summary"] if schema else title["result"]
+            chunk_summary.append(title)
+
+        collected_summaries = " ".join(chunk_summary)
+        return collected_summaries
+
+    def _generate(self, prompt: str, text: str, gen_cfg, schema: str = None) -> str | dict:
+        prompt = prompt.format(text)
         print(f"Generate {prompt=}")
         # If a schema is given, conform to schema
         if schema:
             print(f"Schema {schema=}")
-            import jsonformer
-
-            jsonformer_llm = jsonformer.Jsonformer(model=self.model,
-                                                   tokenizer=self.tokenizer,
-                                                   json_schema=json.loads(schema),
-                                                   prompt=prompt,
-                                                   max_string_token_length=self.gen_cfg.max_new_tokens)
+            jsonformer_llm = self.json_former(model=self.model,
+                                              tokenizer=self.tokenizer,
+                                              json_schema=json.loads(schema),
+                                              prompt=prompt,
+                                              max_string_token_length=gen_cfg.max_new_tokens)
             response = jsonformer_llm()
         else:
             # If no schema, perform prompt only generation
 
             # tokenize prompt
             input_ids = self.tokenizer.encode(prompt, return_tensors="pt").to(
-                self.model.device
+                    self.model.device
             )
-            output = self.model.generate(input_ids, generation_config=self.gen_cfg)
+            output = self.model.generate(input_ids, generation_config=gen_cfg)
 
             # decode output
             response = self.tokenizer.decode(output[0].cpu(), skip_special_tokens=True)
+            response = response[len(prompt):]
         print(f"Generated {response=}")
-        return {"text": response}
+        return {"result": response}
 
+    @method()
+    def generate(self, req) -> str | dict:
+        return self._generation_swivel(req.task)(req)
 
 # -------------------------------------------------------------------
 # Web API
@@ -172,56 +236,15 @@ def web():
 
     class LLMRequest(BaseModel):
         prompt: str
+        text: str
+        task: str
         schema: Optional[dict] = None
 
     @app.post("/llm", dependencies=[Depends(apikey_auth)])
     async def llm(
-        req: LLMRequest,
+            req: LLMRequest,
     ):
-        if req.schema:
-            func = llmstub.generate.spawn(prompt=req.prompt, schema=json.dumps(req.schema))
-        else:
-            func = llmstub.generate.spawn(prompt=req.prompt)
-        result = func.get()
-        return result
-
-    class LLMTitleRequest(BaseModel):
-        prompt: str
-        text: str
-        schema: Optional[dict] = None
-
-    @app.post("/title", dependencies=[Depends(apikey_auth)])
-    async def llm(
-            req: LLMTitleRequest,
-    ):
-        if req.schema:
-            func = llmstub.generate.spawn(prompt=req.prompt,
-                                          text=req.text,
-                                          schema=json.dumps(req.schema))
-        else:
-            func = llmstub.generate.spawn(prompt=req.prompt,
-                                          text=req.text)
-        result = func.get()
-        return result
-
-    class LLMSummaryRequest(BaseModel):
-        prompt: str
-        text: str
-        schema: Optional[dict] = None
-
-    @app.post("/summary", dependencies=[Depends(apikey_auth)])
-    async def llm(
-            req: LLMSummaryRequest,
-    ):
-        if req.schema:
-            func = llmstub.generate.spawn(prompt=req.prompt,
-                                          text=req.text,
-                                          schema=json.dumps(req.schema))
-        else:
-            func = llmstub.generate.spawn(prompt=req.prompt,
-                                          text=req.text)
-        result = func.get()
-        return result
+        return llmstub.generate(req)
 
     @app.post("/warmup", dependencies=[Depends(apikey_auth)])
     async def warmup():
