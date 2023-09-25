10bacd35d380f62bb0a432a624199d29cbac86f2
Sara <sara@monadical.com>
2023-09-25 18:15:40 +0200

    adds about and privacy pages

---
+++ www/app/about.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+import FullscreenModal from "./transcripts/fullsreenModal";
+import AboutContent from "./aboutContent";
+
+type AboutProps = {
+  buttonText: string;
+};
+
+export default function About({ buttonText }: AboutProps) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px] font-light px-2"
+        onClick={() => setModalOpen(true)}
+      >
+        {buttonText}
+      </button>
+      {modalOpen && (
+        <FullscreenModal close={() => setModalOpen(false)}>
+          <AboutContent />
+        </FullscreenModal>
+      )}
+    </>
+  );
+}
