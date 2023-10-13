168e4d6fa4e6b9138f0528e53f3a4b03f9cf416c
Mathieu Virbel <mat@meltingrocks.com>
2023-10-13 09:58:16 +0200

    www: add features for privacy and browsing

--- www/app/lib/utils.ts
+++ www/app/lib/utils.ts
@@ -1,3 +1,11 @@
 export function isDevelopment() {
   return process.env.NEXT_PUBLIC_ENV === "development";
 }
+
+export function featPrivacy() {
+  return process.env.NEXT_PUBLIC_FEAT_PRIVACY === "1";
+}
+
+export function featBrowse() {
+  return process.env.NEXT_PUBLIC_FEAT_BROWSE === "1";
+}
