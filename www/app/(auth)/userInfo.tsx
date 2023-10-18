0e2c98144d190173576734d71960f34c32cb7954
Mathieu Virbel <mat@meltingrocks.com>
2023-10-18 19:52:03 +0200

    www: add login on layout and fixes browse

--- www/app/(auth)/userInfo.tsx
+++ www/app/(auth)/userInfo.tsx
@@ -12,18 +12,16 @@ export default function UserInfo() {
   return !isAuthenticated ? (
     <span className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px] font-light px-2">
       <Link href="/login" className="outline-none">
-        Log in or create account
+        Log in
       </Link>
     </span>
   ) : (
     <span className="font-light px-2">
-      {userinfo?.email} (
       <span className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px]">
         <Link href="/logout" className="outline-none">
           Log out
         </Link>
       </span>
-      )
     </span>
   );
 }
