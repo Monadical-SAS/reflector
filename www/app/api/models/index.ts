0381f060ed9933e739b5a28799bb064cc4fbe14a
Mathieu Virbel <mat@meltingrocks.com>
2023-10-19 21:05:49 +0200

    www: update frontend to support new transcript format in topics

--- www/app/api/models/index.ts
+++ www/app/api/models/index.ts
@@ -7,6 +7,7 @@ export * from "./GetTranscript";
 export * from "./HTTPValidationError";
 export * from "./PageGetTranscript";
 export * from "./RtcOffer";
+export * from "./TranscriptSegmentTopic";
 export * from "./TranscriptTopic";
 export * from "./UpdateTranscript";
 export * from "./UserInfo";
