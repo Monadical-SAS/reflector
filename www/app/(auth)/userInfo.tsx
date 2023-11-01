870b4966bbf30b3106b4c5dfebcef282b4a693e3
Sara <sara@monadical.com>
2023-11-01 17:17:49 +0100

    prevent prefetch of login/logout

--- www/app/(auth)/userInfo.tsx
+++ www/app/(auth)/userInfo.tsx
@@ -7,14 +7,14 @@ export default function UserInfo() {
 
   return !isAuthenticated ? (
     <span className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px] font-light px-2">
-      <Link href="/login" className="outline-none">
+      <Link href="/login" className="outline-none" prefetch={false}>
         Log in
       </Link>
     </span>
   ) : (
     <span className="font-light px-2">
       <span className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px]">
-        <Link href="/logout" className="outline-none">
+        <Link href="/logout" className="outline-none" prefetch={false}>
           Log out
         </Link>
       </span>
