c11db11249d56b7b7eac43e9719db9d636ddaf71
Sara <sara@monadical.com>
2023-11-08 16:24:09 +0100

    remove timeout

--- www/app/(errors)/errorMessage.tsx
+++ www/app/(errors)/errorMessage.tsx
@@ -6,7 +6,6 @@ import * as Sentry from "@sentry/react";
 const ErrorMessage: React.FC = () => {
   const { error, setError, humanMessage } = useError();
   const [isVisible, setIsVisible] = useState<boolean>(false);
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
 
   // Setup Shortcuts
   useEffect(() => {
@@ -39,26 +38,11 @@ const ErrorMessage: React.FC = () => {
     }
   }, [error]);
 
-  useEffect(() => {
-    if (isVisible) {
-      setTimeoutId(
-        setTimeout(() => {
-          setIsVisible(false);
-          setTimeoutId(undefined);
-        }, 30000),
-      );
-    }
-    if (!isVisible && timeoutId) {
-      clearTimeout(timeoutId);
-    }
-  }, [isVisible]);
-
   if (!isVisible || !humanMessage) return null;
 
   return (
     <button
       onClick={() => {
-        setIsVisible(false);
         setIsVisible(false);
       }}
       className="max-w-xs z-50 fixed bottom-5 right-5 md:bottom-10 md:right-10 border-solid bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded transition-opacity duration-300 ease-out opacity-100 hover:opacity-80 focus-visible:opacity-80 cursor-pointer transform hover:scale-105 focus-visible:scale-105"
--- www/app/lib/errorUtils.ts
+++ www/app/lib/errorUtils.ts
@@ -1,6 +1,7 @@
 function shouldShowError(error: Error | null | undefined) {
   if (error?.name == "ResponseError" && error["response"].status == 404)
     return false;
+  if (error?.name == "FetchError") return false;
   return true;
 }
 
