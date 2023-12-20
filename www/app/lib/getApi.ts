c67ba39b12bd2b2db11a54d7602205f1fa027b45
Mathieu Virbel <mat@meltingrocks.com>
2023-12-20 12:02:10 +0100

    WIP

--- www/app/lib/getApi.ts
+++ www/app/lib/getApi.ts
@@ -1,16 +1,15 @@
-import { Configuration } from "../api/runtime";
-import { DefaultApi } from "../api/apis/DefaultApi";
+import { DefaultService, OpenAPI } from "../api";
 
 import { useFiefAccessTokenInfo } from "@fief/fief/nextjs/react";
 import { useContext, useEffect, useState } from "react";
 import { DomainContext, featureEnabled } from "../[domain]/domainContext";
 import { CookieContext } from "../(auth)/fiefWrapper";
 
-export default function getApi(): DefaultApi | undefined {
+export default function getApi(): DefaultService | undefined {
   const accessTokenInfo = useFiefAccessTokenInfo();
   const api_url = useContext(DomainContext).api_url;
   const requireLogin = featureEnabled("requireLogin");
-  const [api, setApi] = useState<DefaultApi>();
+  const [api, setApi] = useState<DefaultService>();
   const { hasAuthCookie } = useContext(CookieContext);
 
   if (!api_url) throw new Error("no API URL");
@@ -20,13 +19,17 @@ export default function getApi(): DefaultApi | undefined {
       return;
     }
 
-    const apiConfiguration = new Configuration({
-      basePath: api_url,
-      accessToken: accessTokenInfo
-        ? "Bearer " + accessTokenInfo.access_token
-        : undefined,
-    });
-    setApi(new DefaultApi(apiConfiguration));
+    // const apiConfiguration = new Configuration({
+    //   basePath: api_url,
+    //   accessToken: accessTokenInfo
+    //     ? "Bearer " + accessTokenInfo.access_token
+    //     : undefined,
+    // });
+    OpenAPI.BASE = api_url;
+    if (accessTokenInfo) {
+      OpenAPI.TOKEN = "Bearer " + accessTokenInfo.access_token;
+    }
+    setApi(DefaultService);
   }, [!accessTokenInfo, hasAuthCookie]);
 
   return api;
