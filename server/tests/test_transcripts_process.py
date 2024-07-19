9b36adedae11d7057a2ba320ced19376cd1a602d
Sergey Mankovsky <sergey@monadical.com>
2024-07-19 12:30:01 +0200

    Fix broken test

--- server/tests/test_transcripts_process.py
+++ server/tests/test_transcripts_process.py
@@ -28,9 +28,9 @@ async def test_transcript_process(
 
     # upload mp3
     response = await ac.post(
-        f"/transcripts/{tid}/record/upload",
+        f"/transcripts/{tid}/record/upload?chunk_number=0&total_chunks=1",
         files={
-            "file": (
+            "chunk": (
                 "test_short.wav",
                 open("tests/records/test_short.wav", "rb"),
                 "audio/mpeg",
