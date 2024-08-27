2e7ae9a6ab7f5fe86f902c3299878918aaab3f48
Sergey Mankovsky <sergey@monadical.com>
2024-08-27 18:54:43 +0200

    Celery task for posting

---
+++ server/reflector/zulip.py
@@ -0,0 +1,72 @@
+from datetime import timedelta
+from urllib.parse import urlparse
+
+import requests
+from reflector.db.transcripts import Transcript
+from reflector.settings import settings
+
+
+def send_message_to_zulip(stream: str, topic: str, message: str):
+    if not stream or not topic or not message:
+        raise ValueError("Missing required parameters")
+
+    try:
+        response = requests.post(
+            f"https://{settings.ZULIP_REALM}/api/v1/messages",
+            data={
+                "type": "stream",
+                "to": stream,
+                "topic": topic,
+                "content": message,
+            },
+            auth=(settings.ZULIP_BOT_EMAIL, settings.ZULIP_API_KEY),
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        response.raise_for_status()
+
+        return response.json()
+    except requests.RequestException as error:
+        raise Exception(f"Failed to send message to Zulip: {error}")
+
+
+def get_zulip_message(transcript: Transcript):
+    domain = (
+        "http://localhost:3000"  # Replace this with your deployment base URL if needed
+    )
+    transcript_url = f"{domain}/transcripts/{transcript.id}"
+
+    header_text = f"# Reflector – {transcript.title or 'Unnamed recording'}\n\n"
+    header_text += f"**Date**: <time:{transcript.created_at.isoformat()}>\n"
+    header_text += f"**Link**: [{extract_domain(transcript_url)}]({transcript_url})\n"
+    header_text += f"**Duration**: {format_time_ms(transcript.duration)}\n\n"
+
+    topic_text = ""
+
+    if transcript.topics:
+        topic_text = "```spoiler Topics\n"
+        for topic in transcript.topics:
+            topic_text += f"1. [{format_time(topic.timestamp)}] {topic.title}\n"
+        topic_text += "```\n\n"
+
+    summary = "```spoiler Summary\n"
+    summary += transcript.long_summary
+    summary += "```\n\n"
+
+    message = header_text + summary + topic_text + "-----\n"
+    return message
+
+
+def extract_domain(url: str) -> str:
+    return urlparse(url).netloc
+
+
+def format_time_ms(milliseconds: float) -> str:
+    return format_time(milliseconds // 1000)
+
+
+def format_time(seconds: float) -> str:
+    td = timedelta(seconds=seconds)
+    time = str(td - timedelta(microseconds=td.microseconds))
+
+    return time[2:] if time.startswith("0:") else time
