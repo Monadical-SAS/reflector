bda4e1cdc09894ebf48b1808ea8735e255638850
Sergey Mankovsky <sergey@monadical.com>
2024-09-02 11:51:58 +0200

    Redirect to past meeting authenticated users

--- www/app/[roomName]/page.tsx
+++ www/app/[roomName]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import "@whereby.com/browser-sdk/embed";
-import { useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import useRoomMeeting from "./useRoomMeeting";
+import { useRouter } from "next/navigation";
+import { useFiefIsAuthenticated } from "@fief/fief/build/esm/nextjs/react";
 
 export type RoomDetails = {
   params: {
@@ -14,10 +16,26 @@ export default function Room(details: RoomDetails) {
   const wherebyRef = useRef<HTMLElement>(null);
   const roomName = details.params.roomName;
   const meeting = useRoomMeeting(roomName);
+  const router = useRouter();
+  const isAuthenticated = useFiefIsAuthenticated();
 
   const roomUrl = meeting?.response?.host_room_url
     ? meeting?.response?.host_room_url
     : meeting?.response?.room_url;
+
+  const handleLeave = useCallback((e) => {
+    router.push("/browse");
+  }, []);
+
+  useEffect(() => {
+    if (!isAuthenticated || !roomUrl) return;
+
+    wherebyRef.current?.addEventListener("leave", handleLeave);
+
+    return () => {
+      wherebyRef.current?.removeEventListener("leave", handleLeave);
+    };
+  }, [handleLeave, roomUrl]);
 
   return (
     <>
