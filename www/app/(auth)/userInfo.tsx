716ce442312ce4fccfa3d6659a5e562223242de8
Mathieu Virbel <mat@meltingrocks.com>
2024-09-04 09:21:07 -0600

    fix: fix global layout overscroll + link colors

--- www/app/(auth)/userInfo.tsx
+++ www/app/(auth)/userInfo.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useSession, signOut, signIn } from "next-auth/react";
-import { Spinner } from "@chakra-ui/react";
-import Link from "next/link";
+import { Spinner, Link } from "@chakra-ui/react";
 
 export default function UserInfo() {
   const { status } = useSession();
@@ -11,28 +10,20 @@ export default function UserInfo() {
   return !sessionReady ? (
     <Spinner size="xs" thickness="1px" className="mx-3" />
   ) : !isAuthenticated ? (
-    <span className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px] font-light px-2">
-      <Link
-        href="/"
-        onClick={() => signIn("authentik")}
-        className="outline-none"
-        prefetch={false}
-      >
-        Log in
-      </Link>
-    </span>
+    <Link
+      href="/"
+      className="font-light px-2"
+      onClick={() => signIn("authentik")}
+    >
+      Log in
+    </Link>
   ) : (
-    <span className="font-light px-2">
-      <span className="hover:underline focus-within:underline underline-offset-2 decoration-[.5px]">
-        <Link
-          href="#"
-          onClick={() => signOut({ callbackUrl: "/" })}
-          className="outline-none"
-          prefetch={false}
-        >
-          Log out
-        </Link>
-      </span>
-    </span>
+    <Link
+      href="#"
+      className="font-light px-2"
+      onClick={() => signOut({ callbackUrl: "/" })}
+    >
+      Log out
+    </Link>
   );
 }
