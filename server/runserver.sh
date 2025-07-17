484c351338da175d0978029a8b5d6d2bb3357c66
Mathieu Virbel <mat@meltingrocks.com>
2025-07-16 18:10:11 -0600

    build: move to uv (#488)
    * build: move to uv
    
    * build: add packages declaration
    
    * build: move to python 3.12, as sentencespiece does not work on 3.13
    
    * ci: remove pre-commit check, will be done in another branch.
    
    * ci: fix name checkout
    
    * ci: update lock and dockerfile
    
    * test: remove event_loop, not needed in python 3.12
    
    * test: updated test due to av returning AudioFrame with 4096 samples instead of 1024
    
    * build: prevent using fastapi cli, because there is no way to set default port
    
    I don't want to pass --port 1250 every time, so back on previous
    approach. I deactivated auto-reload for production.
    
    * ci: remove main.py
    
    * test: fix quirck with httpx

--- server/runserver.sh
+++ server/runserver.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if [ -f "/venv/bin/activate" ]; then
-    source /venv/bin/activate
-fi
-
 if [ "${ENTRYPOINT}" = "server" ]; then
-    alembic upgrade head
-    python -m reflector.app
+    uv run alembic upgrade head
+    uv run -m reflector.app
 elif [ "${ENTRYPOINT}" = "worker" ]; then
-    celery -A reflector.worker.app worker --loglevel=info
+    uv run celery -A reflector.worker.app worker --loglevel=info
 elif [ "${ENTRYPOINT}" = "beat" ]; then
-    celery -A reflector.worker.app beat --loglevel=info
+    uv run celery -A reflector.worker.app beat --loglevel=info
 else
     echo "Unknown command"
 fi
