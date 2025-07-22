24fabe3e861ac04857264be5ec31d3a19804d54d
Mathieu Virbel <mat@meltingrocks.com>
2025-07-22 12:20:26 -0600

    fix: whereby consent not displaying (#505)

--- www/app/providers.tsx
+++ www/app/providers.tsx
@@ -4,11 +4,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import system from "./styles/theme";
 
 import { WherebyProvider } from "@whereby.com/browser-sdk/react";
+import { Toaster } from "./components/ui/toaster";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={system}>
-      <WherebyProvider>{children}</WherebyProvider>
+      <WherebyProvider>
+        {children}
+        <Toaster />
+      </WherebyProvider>
     </ChakraProvider>
   );
 }
