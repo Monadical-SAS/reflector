72709e470419a30f79e6d2c98a453b419848d87b
Mathieu Virbel <mat@meltingrocks.com>
2025-08-01 12:03:37 -0600

    test: remove ensure_casing/nltk

--- server/tests/test_transcripts_rtc_ws.py
+++ server/tests/test_transcripts_rtc_ws.py
@@ -226,8 +226,6 @@ async def test_transcript_rtc_and_websocket_and_fr(
     dummy_diarization,
     dummy_storage,
     fake_mp3_upload,
-    ensure_casing,
-    nltk,
     appserver,
 ):
     # goal: start the server, exchange RTC, receive websocket events
