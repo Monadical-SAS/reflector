48403a38b02f760d94358e8b5f63fb541b504d3c
Mathieu Virbel <mat@meltingrocks.com>
2025-08-13 16:09:42 -0600

    style: deactivate PLC0415 only on part that it's ok
    + re-run pre-commit run --all

--- server/reflector/db/utils.py
+++ server/reflector/db/utils.py
@@ -4,4 +4,4 @@ from reflector.db import database
 
 
 def is_postgresql() -> bool:
-    return database.url.scheme and database.url.scheme.startswith('postgresql')
+    return database.url.scheme and database.url.scheme.startswith("postgresql")
--- server/reflector/storage/__init__.py
+++ server/reflector/storage/__init__.py
@@ -1,9 +1,9 @@
 from .base import Storage  # noqa
+from reflector.settings import settings
 
 
 def get_transcripts_storage() -> Storage:
-    from reflector.settings import settings
-
+    assert settings.TRANSCRIPT_STORAGE_BACKEND
     return Storage.get_instance(
         name=settings.TRANSCRIPT_STORAGE_BACKEND,
         settings_prefix="TRANSCRIPT_STORAGE_",
