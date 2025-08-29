f5313006080c6b87fd2903b067dddc8a112f1434
Mathieu Virbel <mat@meltingrocks.com>
2025-08-29 09:34:28 -0600

    fix: alembic revision

--- server/migrations/versions/0194f65cd6d3_add_webhook_fields_to_rooms.py
+++ server/migrations/versions/0194f65cd6d3_add_webhook_fields_to_rooms.py
@@ -1,7 +1,7 @@
 """Add webhook fields to rooms
 
 Revision ID: 0194f65cd6d3
-Revises: 0ab2d7ffaa16
+Revises: 5a8907fd1d78
 Create Date: 2025-08-27 09:03:19.610995
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "0194f65cd6d3"
-down_revision: Union[str, None] = "0ab2d7ffaa16"
+down_revision: Union[str, None] = "5a8907fd1d78"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
