0751d01f136867af9c06b2713f0fd945c37badb0
Igor Loskutov <igor.loskutoff@gmail.com>
2025-09-04 13:37:10 -0400

    added vs edited room state cleanup

--- www/app/(app)/rooms/page.tsx
+++ www/app/(app)/rooms/page.tsx
@@ -189,6 +189,7 @@ export default function RoomsList() {
   const handleCloseDialog = () => {
     setShowWebhookSecret(false);
     setWebhookTestResult(null);
+    setEditRoomId(null);
     onClose();
   };
 
