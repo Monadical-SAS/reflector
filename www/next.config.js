b5ef424807a4b9463b3ae2ae9f604aaee878bb92
Igor Loskutov <igor.loskutoff@gmail.com>
2025-09-06 01:40:46 -0400

    sync backend and frontend token refresh logic

--- www/next.config.js
+++ www/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  reactStrictMode: false,
   experimental: { esmExternals: "loose" },
   env: {
     IS_CI: process.env.IS_CI,
