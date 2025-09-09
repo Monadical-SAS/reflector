abfcc2070198472aff6065273e5adc22086b59a1
Igor Loskutov <igor.loskutoff@gmail.com>
2025-09-09 16:23:45 -0400

    fix: auth post

--- www/app/lib/apiClient.tsx
+++ www/app/lib/apiClient.tsx
@@ -14,9 +14,27 @@ export const client = createClient<paths>({
   baseUrl: API_URL,
 });
 
-// has to be called BEFORE $api is created with createFetchClient<paths>(client) or onRequest doesn't fire [at least for POST]
+const waitForAuthTokenDefinitivePresenceOrAbscence = async () => {
+  let tries = 0;
+  let time = 0;
+  const STEP = 100;
+  while (currentAuthToken === undefined) {
+    await new Promise((resolve) => setTimeout(resolve, STEP));
+    time += STEP;
+    tries++;
+    // most likely first try is more than enough, if it's more there's already something weird happens
+    if (tries > 10) {
+      // even when there's no auth assumed at all, we probably should explicitly call configureApiAuth(null)
+      throw new Error(
+        `Could not get auth token definitive presence/absence in ${time}ms. not calling configureApiAuth?`,
+      );
+    }
+  }
+};
+
 client.use({
-  onRequest({ request }) {
+  async onRequest({ request }) {
+    await waitForAuthTokenDefinitivePresenceOrAbscence();
     if (currentAuthToken) {
       request.headers.set("Authorization", `Bearer ${currentAuthToken}`);
     }
@@ -35,9 +53,9 @@ client.use({
 
 export const $api = createFetchClient<paths>(client);
 
-let currentAuthToken: string | null | undefined = null;
+let currentAuthToken: string | null | undefined = undefined;
 
 // the function contract: lightweight, idempotent
-export const configureApiAuth = (token: string | null | undefined) => {
+export const configureApiAuth = (token: string | null) => {
   currentAuthToken = token;
 };
